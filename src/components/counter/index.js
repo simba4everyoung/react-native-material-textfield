4fff5cb340db24b59797199ddd02fcefee06e376
Simba Xu <simba.xu@soulfx.com>
2017-12-21 15:40:35 -0500

    added countColor props to counter

--- src/components/counter/index.js
+++ src/components/counter/index.js
@@ -14,14 +14,16 @@ export default class Counter extends PureComponent {
     baseColor: PropTypes.string.isRequired,
     errorColor: PropTypes.string.isRequired,
 
+    countColor: PropTypes.string,
+
     style: Text.propTypes.style,
   };
 
   render() {
-    let { count, limit, baseColor, errorColor, fontSize, style } = this.props;
+    let { count, limit, countColor, errorColor, fontSize, style } = this.props;
 
     let textStyle = {
-      color: count > limit? errorColor : baseColor,
+      color: count > limit? errorColor : countColor,
       fontSize,
     };
 
